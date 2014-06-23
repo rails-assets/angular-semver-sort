6aeac2f86750c6bb76e8e407fca65418b5f3a8da
Jason Jarrett <jason@elegantcode.com>
2014-06-22 23:11:54 -0700

    Handle a null/undefined collection

--- src/angular-semver-sort.js
+++ src/angular-semver-sort.js
@@ -9,7 +9,7 @@
     var WARNING_NONSEMVER = 'This collection contains non-semver strings. ' + WARNING_FALLBACK;
 
     var sortAlphabetically = function(collection, prop) {
-      var items = collection.slice(0);
+      var items = (collection || []).slice(0);
       return !prop ? items.sort() : items.sort(function(A, B) {
         var a = A[prop], b = B[prop];
         if ( a > b ) return 1;
